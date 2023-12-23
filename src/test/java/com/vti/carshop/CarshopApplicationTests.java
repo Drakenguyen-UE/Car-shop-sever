b073f14043810af85c3cc3f940d27bec8fecd564
Nhân <nhan652011@gmail.com>
2023-12-23 20:53:28 +0700

    [Add] carshop project

---
+++ src/test/java/com/vti/carshop/CarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
